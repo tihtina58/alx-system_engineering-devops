64b5766b051380fd3577687e175153ad7a394e4d
Tihitna <tihitnasete58@gmail.com>
2022-01-31 05:37:23 -0800

    commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,27 @@
-#!/usr/bin/python3
-"""prints information about employee and his/her tasks using employee id."""
+#!/usrbin/python3
+
+"""
+This module gets the todo of a user identified by id
+gotten from the api https://jsonplaceholder.typicode.com/users
+"""
+
 import requests
-from sys import argv
+import sys
 
 if __name__ == '__main__':
-    emp_id = argv[1]
-    route = "https://jsonplaceholder.typicode.com/users/{}".format(emp_id)
-    r = requests.get(route)
-    usr_info = r.json()
-    emp_name = usr_info.get('name')
-
-    url = "https://jsonplaceholder.typicode.com/users/{}/todos"
-    route = url.format(emp_id)
-    r = requests.get(route)
-    tasks = r.json()
-
-    done_tasks = 0
-    for task in tasks:
-        if task.get('completed') is True:
-            done_tasks += 1
-
-    tot_tasks = len(tasks)
-
-    print("Employee {} is done with tasks({}/{}):"
-          .format(emp_name, done_tasks, tot_tasks))
-
-    for task in tasks:
-        if task.get('completed') is True:
-            print("\t {}".format(task.get('title')))
+    userid = int(sys.argv[1])
+    resp = requests.get(
+        'https://jsonplaceholder.typicode.com/users/{}'.format(userid))
+    name = resp.json()['name']
+    task_resp = requests.get(
+        'https://jsonplaceholder.typicode.com/todos/?userId={}'.format(userid))
+    c_task = 0
+    task_json = task_resp.json()
+    t_len = len(task_json)
+    completed_task_str = ''
+    for i in task_json:
+        if i['completed']:
+            c_task += 1
+            completed_task_str += ('\t{}\n'.format(i['title']))
+    print('Employee {} is done with tasks({}/{}):'.format(name, c_task, t_len))
+    print(completed_task_str)
