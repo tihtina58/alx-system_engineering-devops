bcd52c2154c26894d7e26a1d1b8f0e2ab7898be8
Tihitna <tihitnasete58@gmail.com>
2022-01-31 05:52:13 -0800

    commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,14 +1,85 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
-import requests
-import sys
+"""Module fetches employe tasks and display on stdout."""
+
+
+class Fetch:
+    """Class provides fetch services."""
+
+    def __init__(self, userId):
+        """Initialize fetch instance."""
+
+        payload = {"userId": userId}
+
+        self.userName = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{:s}".format(
+                userId
+            )
+        ).json().get("name")
+
+        self.todos = requests.get(
+            "https://jsonplaceholder.typicode.com/todos/",
+            params=payload
+        ).json()
+        return None
+
+    def task_one(self):
+        """Returns information about employee TODO list progress
+        Args:
+            self (object): <class 'main.Fetch'> type object
+        Returns:
+            Comperhensive string of employee TODO list progress
+        """
+        brief = "Employee {:s} is done with tasks ({:d}/{:d}):\n".format(
+            self.userName,
+            len(Fetch.__tasks_count(self.todos).get("complete")),
+            len(self.todos)
+        )
+        completed = Fetch.__completed_tasks(
+            Fetch.__tasks_count(self.todos).get("complete")
+        )
+        return [brief, completed]
+
+    @staticmethod
+    def __tasks_count(tasks_list):
+        """returns dictionary of task lists acording to completeness.
+        Args:
+            tasks_list (list): List of tasks fetched.
+            status (str): `finished` or `unfinished`.
+        Returns:
+            dictionary of completed and uncompleted task lists.
+        """
+        status_dict = {
+            "complete": [],
+            "incomplete": []
+        }
+
+        for elem in tasks_list:
+            if elem.get("completed") is True:
+                status_dict.get("complete").append(elem)
+        for elem in tasks_list:
+            if elem not in status_dict["complete"]:
+                status_dict.get("incomplete").append(elem)
+        return status_dict
+
+    @staticmethod
+    def __completed_tasks(tasks_list):
+        """Returns string representation of all tasks in provided list
+        Args:
+            tasks_list (list): List of tasks fetched.
+        Returns:
+            Tabulated list of completed tasks
+        """
+        brief = ""
+        for elem in tasks_list:
+            brief += "\t {:s}\n".format(elem.get("title"))
+        return brief
+    pass
+
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
-    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
+    import requests
+    import sys
 
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
-    [print("\t {}".format(c)) for c in completed]
+    fetch = Fetch(sys.argv[1])
+    for elem in fetch.task_one():
+        print(elem, end="")
